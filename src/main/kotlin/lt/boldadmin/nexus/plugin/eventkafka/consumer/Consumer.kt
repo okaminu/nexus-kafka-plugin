9ba1f7636b7722578097ace5a0c546f718a45951
Okaminu <aurimasdgt@gmail.com>
2019-09-11 15:23:18 +0300

    BA-818: fixed following issues: nexus-backend and sektor-backend need kafka-client dependency to have serializer on classpath, spring cannot load property provider because introspection fails)

--- src/main/kotlin/lt/boldadmin/nexus/plugin/eventkafka/consumer/Consumer.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/eventkafka/consumer/Consumer.kt
@@ -1,13 +1,13 @@
 package lt.boldadmin.nexus.plugin.eventkafka.consumer
 
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import lt.boldadmin.nexus.plugin.eventkafka.factory.KafkaConsumerFactory
 import java.time.Duration.ofSeconds
 import java.util.*
 
-open class Consumer {
+open class Consumer(private val consumerFactory: KafkaConsumerFactory) {
 
     fun <T>consume(topic: String, function: (T) -> Unit, properties: Properties) {
-        val consumer = KafkaConsumer<String, T>(properties)
+        val consumer = consumerFactory.create<T>(properties)
         consumer.subscribe(listOf(topic))
 
         executeInfinitely { consumer.poll(ofSeconds(1)).forEach { function(it.value()) } }
